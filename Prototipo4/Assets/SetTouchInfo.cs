030dd63b64981be00372557ee97d38cc6d4b2a93
Unknown <ktalan_force2@hotmail.com>
2018-06-20 23:27:49 -0400

    Prototype 4

---
+++ Prototipo4/Assets/SetTouchInfo.cs
@@ -0,0 +1,91 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SetTouchInfo : MonoBehaviour {
+
+	float previus_distance;
+	public Text Swape_text;
+
+	Vector2 firstTouchposs;
+	Vector2 secondTouchposs;
+	Vector2 currentSwipe;
+	float minSwipeLenght = 200f;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+		if (Input.touchCount > 0) {
+		
+			Touch t = Input.GetTouch (0);
+
+			if (t.phase == TouchPhase.Began) {
+				firstTouchposs = new Vector2 (t.position.x, t.position.y);
+			}
+
+			if (t.phase == TouchPhase.Ended) {
+			
+				secondTouchposs = new Vector2 (t.position.x, t.position.y);
+				currentSwipe = new Vector2 (secondTouchposs.x - firstTouchposs.x, secondTouchposs.y - firstTouchposs.y);
+
+				if (currentSwipe.magnitude < minSwipeLenght) {
+					Swape_text.text = "Swape corto";
+					return;
+				}
+
+				currentSwipe.Normalize ();
+
+				if (currentSwipe.y > 0 && currentSwipe.x > -0.5f && currentSwipe.x < 0.5f) {
+					Swape_text.text = "Swape arriba";
+				} else if (currentSwipe.y < 0 && currentSwipe.x > -0.5f && currentSwipe.x < 0.5f) {
+					Swape_text.text = "Swape abajo";
+				} else if (currentSwipe.x < 0 && currentSwipe.y > -0.5f && currentSwipe.y < 0.5f) {
+					Swape_text.text = "Swape izquierda";
+				} else if (currentSwipe.x > 0 && currentSwipe.y > -0.5f && currentSwipe.y < 0.5f) {
+					Swape_text.text = "Swape derecha";
+				}
+			}
+
+
+
+		}
+
+
+
+		if (Input.touchCount == 2 && (Input.GetTouch(0).phase == TouchPhase.Began || Input.GetTouch(1).phase == TouchPhase.Began)) {
+			previus_distance = Vector2.Distance (Input.GetTouch (0).position, Input.GetTouch (1).position);
+
+		}
+
+		else if(Input.touchCount == 2 && (Input.GetTouch(0).phase == TouchPhase.Moved || Input.GetTouch(1).phase == TouchPhase.Moved)){
+			float distance;
+			Vector2 touch1 = Input.GetTouch (0).position;
+			Vector2 touch2 = Input.GetTouch (1).position;
+
+			distance = Vector2.Distance (touch1, touch2);
+
+			float pinch = previus_distance - distance;
+
+
+			previus_distance = distance;
+
+
+
+			if (pinch > 5) {
+				Swape_text.text = "Zoom_in";
+
+			} else if (pinch < -5) {
+				Swape_text.text = "Zoom_out";
+
+			}
+
+		}
+		
+	}
+}
